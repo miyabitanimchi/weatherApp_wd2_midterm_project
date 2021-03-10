f18deb47741aaede7b6fdecbdb65680ff188cb6d
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-03-09 16:39:49 -0800

    added some more functions

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const api = {
 
 // get DOM
 const searchCity = document.getElementById("searchCityButton");
-let now = new Date();
-let date = document.getElementById("date");
+let now;
+let realTimeDate = document.getElementById("date");
 let city = document.getElementById("city-name");
 let weatherDescri = document.getElementById("weather-description");
 let weatherIcon = document.getElementById("weather-icon");
@@ -15,7 +15,7 @@ let currentTemp = document.getElementById("current-temp");
 let feelsLike = document.getElementById("feelslike-temp");
 
 
-// fn for when you search a city
+// function for when you search a city
 const getResults = (city) => {
     fetch(`${api.base}weather?q=${city}&units=metric&appid=${api.key}`).then((res) => {
         console.log(res);
@@ -23,27 +23,63 @@ const getResults = (city) => {
             alert(`We've got a problem. The status code: ${res.status} `);
             return;
         }
-        res.json().then((data) => {
-            console.log(data);
+        res.json().then((cityData) => {
+            // setInterval("createElements(cityData)", 1000);
+            createElements(cityData);
+            console.log(cityData);
         })
     }).catch((err) => {
         console.log(`error ${err}`);
     })
 }
-getResults("Toronto");
+
 
 // Get current date from "new Date()" and build a date to be shown
-const dateBuilder = (d) => {
+const dateBuilder = () => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    let day = days[d.getDay()];
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`;
+    now = new Date();
+    let day = days[now.getDay()];
+    let date = now.getDate();
+    let month = months[now.getMonth()];
+    let year = now.getFullYear();
+    let hour = makeDigitDouble(now.getHours());
+    let time = makeDigitDouble(now.getMinutes());
+    let second = makeDigitDouble(now.getSeconds());
 
+    realTimeDate.textContent = (date === 1 || date === 21 || date === 31) ? `${day}, ${month} ${date}st, ${year} ${hour} : ${time} ${second}`
+        : (date === 2 || date === 22) ? `${day}, ${month}  ${date}nd, ${year} ${hour} : ${time} ${second}`
+        : (date === 3 || date === 23) ? `${day}, ${month}  ${date}rd, ${year} ${hour} : ${time} ${second}`
+        : `${day}, ${month} ${date}th, ${year} ${hour}:${time} ${second}`
+
+    
+    // return `${day} ${date} ${month} ${year} ${hour} : ${time}`;
 };
+
+// Update the information every one second (because of the second)
+setInterval(dateBuilder, 1000);
+
+// function to put 0 for an hour and time when they are single digits
+const makeDigitDouble = (num) => {
+    num += "";
+    return (num.length === 1) ? num = "0" + num : num;
+};
+
+
+// Create elements to show them on html
+const createElements = (cityData) => {
+    // createElements(cityData);
+    // let now = new Date(cityData.dt * 1000);
+    let iconCode = cityData.weather[0].icon;
+    let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+
+    weatherDescri.innerHTML = cityData.weather[0].main;
+    weatherIcon.innerHTML = `<img src="${iconUrl}" alt=""></img>`;
+    
+    currentTemp.innerHTML = `${Math.round(cityData.main.temp)}<span>℃</span>`;
+}
+
 
 // when you visit the page
 window.addEventListener("load", getResults("Vancouver"));
-
